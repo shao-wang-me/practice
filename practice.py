306c1a2f4b92dd78f5b145d6a07d8e3cd06b89a5
Shao Wang <10136589+shao-wang-me@users.noreply.github.com>
2022-08-11 22:40:19 +1000

    640. Solve the Equation

--- practice.py
+++ practice.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import date
 import json
 
 @dataclass(frozen=True, order=True)
@@ -14,7 +14,7 @@ class Challenge:
 @dataclass(frozen=True, order=True)
 class Attempt:
   challenge: Challenge
-  time: datetime
+  date: date
   rating: int
 
 def load_data() -> tuple[set[Tag], set[Challenge], set[Attempt]]:
@@ -22,16 +22,16 @@ def load_data() -> tuple[set[Tag], set[Challenge], set[Attempt]]:
     data = json.load(f)
     tags = {Tag(name=x['name']) for x in data['tags']}
     challenges = {Challenge(name=x['name'], tags=frozenset(t for t in tags if t.name in x['tags'])) for x in data['challenges']}
-    attemps = {Attempt(challenge=[c for c in challenges if c.name in x['challenge']][0], time=datetime.fromisoformat(x['time']), rating=x['rating']) for x in data['attempts']}
+    attemps = {Attempt(challenge=[c for c in challenges if c.name in x['challenge']][0], date=date.fromisoformat(x['time']), rating=x['rating']) for x in data['attempts']}
     return tags, challenges, attemps
 
-def report(tags, challenges, attempts):
+def report(tags: set[Tag], challenges: set[Challenge], attempts: set[Attempt]) -> None:
   """Get scores for challenges and tags"""
   challenge_scores = {c: 0 for c in challenges}
   for a in attempts:
     # Attempt rating is from 1 to 5, and we decrease it by 1 every week for the scoring
     # Challenge scoring is a sum of scoring of all attempts
-    challenge_scores[a.challenge] += max(0, a.rating - (datetime.now() - a.time).days // 7)
+    challenge_scores[a.challenge] += max(0, a.rating - (date.today() - a.date).days // 7)
   # Tag scoring is a sum of scoring of all challenges that have the tag
   tag_scores = {t: 0 for t in tags}
   for c in challenges:
@@ -46,7 +46,7 @@ def report(tags, challenges, attempts):
   for t, score in sorted(tag_scores.items(), key=lambda x: x[1], reverse=True):
     print(f'  {t.name}: {score}')
 
-def main():
+def main() -> None:
   tags, challenges, attempts = load_data()
   report(tags=tags, challenges=challenges, attempts=attempts)
 
